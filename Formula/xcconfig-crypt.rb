43f0c2d48fc6d8cbe43966ab2ff34a8e13c2c015
Kiel Oleson <kiel.oleson@thriveglobal.com>
2020-12-04 11:34:23 -0800

    add xcconfig-crypt, the first formula.

---
+++ Formula/xcconfig-crypt.rb
@@ -0,0 +1,17 @@
+class XcconfigCrypt < Formula
+  desc "Symmetrically encrypt/decrypt .xcconfig files in a diffable fashion"
+  homepage "https://github.com/koleson/xcconfig-crypt"
+  url "https://github.com/koleson/xcconfig-crypt.git", tag: "0.1.2"
+  license "MIT"
+  head "https://github.com/koleson/xcconfig-crypt.git"
+
+  depends_on xcode: ["12.0", :build]
+
+  def install
+    system "make", "install", "prefix=#{prefix}"
+  end
+
+  test do
+    system "#{bin}/xcconfig-crypt" "-h"
+  end
+end
