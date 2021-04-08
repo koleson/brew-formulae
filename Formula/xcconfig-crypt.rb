e969f21c9185cc00f554e9e72aaed24b64c70714
Kiel Oleson <kiel@jusquici.org>
2021-04-07 20:00:59 -0700

    bump xcconfig-crypt version to trigger bottle build

--- Formula/xcconfig-crypt.rb
+++ Formula/xcconfig-crypt.rb
@@ -1,7 +1,7 @@
 class XcconfigCrypt < Formula
   desc "Symmetrically encrypt/decrypt .xcconfig files in a diffable fashion"
   homepage "https://github.com/koleson/xcconfig-crypt"
-  url "https://github.com/koleson/xcconfig-crypt.git", tag: "0.1.5"
+  url "https://github.com/koleson/xcconfig-crypt.git", tag: "0.1.6"
   license "MIT"
   head "https://github.com/koleson/xcconfig-crypt.git"
 
