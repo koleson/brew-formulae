ff44cdd9208eaa0b508ccf9266fcf21d32b42363
Kiel <569012+koleson@users.noreply.github.com>
2021-04-08 04:55:38 +0000

    xcconfig-crypt: add 0.1.7 bottle.

--- Formula/xcconfig-crypt.rb
+++ Formula/xcconfig-crypt.rb
@@ -5,6 +5,11 @@ class XcconfigCrypt < Formula
   license "MIT"
   head "https://github.com/koleson/xcconfig-crypt.git"
 
+  bottle do
+    root_url "https://github.com/koleson/homebrew-formulae/releases/download/xcconfig-crypt-0.1.7"
+    sha256 cellar: :any_skip_relocation, catalina: "8f18ecef52cc90a8513823023ea137a6329e64e4f5a91408f53990d75008aa98"
+  end
+
   depends_on xcode: ["12.0", :build]
 
   def install
